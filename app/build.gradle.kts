c332015e02af29095dcd549b84c7a77df4084380
yarShpep <112943420+yarShpep@users.noreply.github.com>
2026-09-09 17:12:42 +0300

    Bump version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.example.atvretranslation"
         minSdk = 23
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.6.0"
+        versionCode = 14
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
